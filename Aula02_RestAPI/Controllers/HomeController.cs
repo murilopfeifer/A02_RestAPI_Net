bf92b0254d755c6b734437ad697fe54d339a6edd
Murilo Pfeifer <murilosipfti@gmail.com>
2022-01-04 22:24:28 -0300

    first commit

---
+++ Aula02_RestAPI/Controllers/HomeController.cs
@@ -0,0 +1,114 @@
+﻿using Aula02_RestAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Aula02_RestAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Pessoas()
+        {
+            string BaseUrl = "https://localhost:7059/";
+
+            List<Pessoa> pessoas = new List<Pessoa>();
+
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add
+                (new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/pessoas");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dados = response.Content.ReadAsStringAsync().Result;
+                pessoas = JsonConvert.DeserializeObject<List<Pessoa>>(dados);
+            }
+            return View(pessoas);
+        }
+
+        public async Task<ActionResult> PessoaId(int id)
+        {
+            string BaseUrl = "https://localhost:7059/";
+            Pessoa? p = new Pessoa();
+
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add
+                (new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("api/pessoas/" + id);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var dados = response.Content.ReadAsStringAsync().Result;
+                p = JsonConvert.DeserializeObject<Pessoa>(dados);
+            }
+            return View(p);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastrar(Pessoa p)
+        {
+            string BaseUrl = "https://localhost:7059/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(BaseUrl + "api/pessoas", p);
+
+            return RedirectToAction("pessoas");
+
+        }
+
+        public IActionResult Alterar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Alterar(Pessoa p, int id)
+        {
+            string BaseUrl = "https://localhost:7059/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.PutAsJsonAsync(BaseUrl + "api/pessoas/" + id, p);
+
+            return RedirectToAction("pessoas");
+        }
+
+        public IActionResult Excluir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Excluir(int id)
+        {
+            string BaseUrl = "https://localhost:7059/";
+
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.DeleteAsync(BaseUrl + "api/pessoas/" + id);
+
+            return RedirectToAction("pessoas");
+        }
+
+
+
+    }
+}
---
+++ Aula02_RestAPI/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula02_RestAPI.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nome da pessoa:")]
+        [StringLength(100, MinimumLength = 5)]
+        public string nome { get; set; }
+    }
+}
